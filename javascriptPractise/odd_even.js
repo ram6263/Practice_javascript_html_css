cc9f8d49313ec34f833b0d577e195ea804fcfb1b
ram6263 <113287847+ram6263@users.noreply.github.com>
2025-09-13 08:58:14 +0530

    Add new file

---
+++ javascriptPractise/odd_even.js
@@ -0,0 +1,10 @@
+function oddAndEven(num){
+    if(num%2===0){
+        console.log("Number is Even")
+    }
+    else{
+        console.log("Number is Odd")
+    }
+}
+// let val=prompt("enter a number");
+oddAndEven(2)
